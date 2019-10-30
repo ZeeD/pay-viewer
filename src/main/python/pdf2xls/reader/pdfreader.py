fe17059f578414d14364bc6c2f55ab6e402fc83d
Vito De Tullio <vito.detullio@gmail.com>
2019-10-30 06:20:13 +0100

    add calcolo ferie / rol

--- src/main/python/pdf2xls/reader/pdfreader.py
+++ src/main/python/pdf2xls/reader/pdfreader.py
@@ -38,6 +38,7 @@ class PdfReader(abcreader.ABCReader):
         table_money = tables[5]
         # table_meta = tables[2]
         table_netto_da_pagare = tables[9]
+        table_ferie = tables[10]
 
         when = extract_periodo(table_periodo)
 
@@ -50,6 +51,15 @@ class PdfReader(abcreader.ABCReader):
 
         netto_da_pagare = extract_netto_da_pagare(table_netto_da_pagare)
 
+        ferie_a_prec = extract_ferie_a_prec(table_ferie)
+        ferie_spett = extract_ferie_spett(table_ferie)
+        ferie_godute = extract_ferie_godute(table_ferie)
+        ferie_saldo = extract_ferie_saldo(table_ferie)
+        par_a_prec = extract_par_a_prec(table_ferie)
+        par_spett = extract_par_spett(table_ferie)
+        par_godute = extract_par_godute(table_ferie)
+        par_saldo = extract_par_saldo(table_ferie)
+
         self.cached_infos = [
             info.Info(when, minimo, keys.Keys.minimo),
             info.Info(when, scatti, keys.Keys.scatti),
@@ -57,7 +67,15 @@ class PdfReader(abcreader.ABCReader):
             info.Info(when, sup_ass, keys.Keys.sup_ass),
             info.Info(when, edr, keys.Keys.edr),
             info.Info(when, totale_retributivo, keys.Keys.totale_retributivo),
-            info.Info(when, netto_da_pagare, keys.Keys.netto_da_pagare)
+            info.Info(when, netto_da_pagare, keys.Keys.netto_da_pagare),
+            info.Info(when, ferie_a_prec, keys.Keys.ferie_a_prec),
+            info.Info(when, ferie_spett, keys.Keys.ferie_spett),
+            info.Info(when, ferie_godute, keys.Keys.ferie_godute),
+            info.Info(when, ferie_saldo, keys.Keys.ferie_saldo),
+            info.Info(when, par_a_prec, keys.Keys.par_a_prec),
+            info.Info(when, par_spett, keys.Keys.par_spett),
+            info.Info(when, par_godute, keys.Keys.par_godute),
+            info.Info(when, par_saldo, keys.Keys.par_saldo)
         ]
         return self.cached_infos
 
@@ -89,6 +107,8 @@ def extract(el: typing.Union[str, float]) -> decimal.Decimal:
             s = round(el, 2)
     else:
         s = el.replace('.', '').replace(',', '.')
+        if s.endswith('-'):
+            s = s[-1] + s[:-1]
 
     return decimal.Decimal(s)
 
@@ -119,3 +139,36 @@ def extract_totale_retributivo(table: pandas.DataFrame) -> decimal.Decimal:
 
 def extract_netto_da_pagare(table: pandas.DataFrame) -> decimal.Decimal:
     return extract(table[0][0].split()[0])
+
+
+def extract_ferie_a_prec(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[1][1])
+
+
+def extract_ferie_spett(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[2][1])
+
+
+def extract_ferie_godute(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[3][1])
+
+
+def extract_ferie_saldo(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[4][1])
+
+
+def extract_par_a_prec(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[1][2])
+
+
+def extract_par_spett(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[2][2])
+
+
+def extract_par_godute(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[3][2])
+
+
+def extract_par_saldo(table: pandas.DataFrame) -> decimal.Decimal:
+    return extract(table[4][2])
+
