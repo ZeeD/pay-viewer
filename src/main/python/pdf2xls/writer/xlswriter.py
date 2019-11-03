ec771da6de4a456e1f67470038998e60bc21988e
Vito De Tullio <vito.detullio@gmail.com>
2019-11-03 08:47:05 +0100

    add formatting

--- src/main/python/pdf2xls/writer/xlswriter.py
+++ src/main/python/pdf2xls/writer/xlswriter.py
@@ -5,10 +5,37 @@ import decimal
 import typing
 
 import openpyxl
+import openpyxl.cell
 
 from . import abcwriter
 from ..model import info
 from ..model import keys
+
+NUMBER_FORMAT_TEXT = '@'
+NUMBER_FORMAT_DATE = 'mm-dd-yy'
+NUMBER_FORMAT_NUMBER = '0.00'
+NUMBER_FORMAT_CURRENCY = r'#,##0.00\ "€"'
+
+VALUE_NUMBER_FORMAT = {
+    keys.Keys.periodo: NUMBER_FORMAT_NUMBER,
+    keys.Keys.livello_categoria: NUMBER_FORMAT_NUMBER,
+    keys.Keys.n_scatti: NUMBER_FORMAT_NUMBER,
+    keys.Keys.minimo: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.scatti: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.superm: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.sup_ass: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.edr: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.totale_retributivo: NUMBER_FORMAT_CURRENCY,
+    keys.Keys.ferie_a_prec: NUMBER_FORMAT_NUMBER,
+    keys.Keys.ferie_spett: NUMBER_FORMAT_NUMBER,
+    keys.Keys.ferie_godute: NUMBER_FORMAT_NUMBER,
+    keys.Keys.ferie_saldo: NUMBER_FORMAT_NUMBER,
+    keys.Keys.par_a_prec: NUMBER_FORMAT_NUMBER,
+    keys.Keys.par_spett: NUMBER_FORMAT_NUMBER,
+    keys.Keys.par_godute: NUMBER_FORMAT_NUMBER,
+    keys.Keys.par_saldo: NUMBER_FORMAT_NUMBER,
+    keys.Keys.netto_da_pagare: NUMBER_FORMAT_CURRENCY
+}
 
 
 class XlsWriter(abcwriter.ABCWriter):
@@ -17,8 +44,10 @@ class XlsWriter(abcwriter.ABCWriter):
     def __init__(self, info_file: typing.BinaryIO) -> None:
         super().__init__(info_file)
         self.workbook = openpyxl.Workbook(write_only=True)
-        self.sheet = self.workbook.create_sheet()
-        self.sheet.append(['month'] + sorted(key.name for key in keys.Keys))
+        self.sheet = self.workbook.create_sheet('main')
+        self.sheet.append([self._cell('month', NUMBER_FORMAT_TEXT)] + 
+                          [self._cell(key.name, NUMBER_FORMAT_TEXT)
+                           for key in keys.Keys])
 
     def write_feature_infos(self,
                             feature: keys.Keys,
@@ -33,14 +62,17 @@ class XlsWriter(abcwriter.ABCWriter):
 
         for when in sorted(self.table):
             features: typing.Dict[str, decimal.Decimal] = self.table[when]
-
-            row: typing.List[typing.Optional[typing.Union[decimal.Decimal, datetime.date]]] = []
-            row += [when]
-            row += [
-                features[feature] if feature in features else None
-                for feature in sorted(key.name for key in keys.Keys)
-            ]
-
-            self.sheet.append(row)
+            self.sheet.append([self._cell(when, NUMBER_FORMAT_DATE)] + 
+                              [self._cell(features.get(key.name, None),
+                                          VALUE_NUMBER_FORMAT[key])
+                               for key in keys.Keys])
 
         self.workbook.save(typing.cast(typing.BinaryIO, self.info_file))
+
+    def _cell(self,
+              value: typing.Union[None, str, datetime.date, decimal.Decimal],
+              number_format: str
+              ) -> openpyxl.cell.Cell:
+        cell = openpyxl.cell.Cell(self.sheet, value=value)
+        cell.number_format = number_format  # pylint: disable=assigning-non-slot
+        return cell
