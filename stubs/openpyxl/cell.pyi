4e4aae4ff24597fb5765ac328b8a964decccfe34
Vito De Tullio <vito.detullio@gmail.com>
2021-02-18 10:12:55 +0100

    stubs and xls export refactor

--- stubs/openpyxl/cell.pyi
+++ stubs/openpyxl/cell.pyi
@@ -2,8 +2,11 @@ import typing
 
 import openpyxl.worksheet.worksheet
 
+
 class Cell:
     number_format: str
+    column_letter: str
+    value: typing.Any
 
     def __init__(self,
                  worksheet: openpyxl.worksheet.worksheet.Worksheet,
--- stubs/openpyxl/worksheet/worksheet.pyi
+++ stubs/openpyxl/worksheet/worksheet.pyi
@@ -2,4 +2,7 @@
 
 import typing
 
-Worksheet = typing.List[typing.Iterable[typing.Any]]
+from .dimensions import ColumnDimension
+
+class Worksheet(typing.List[typing.Iterable[typing.Any]]):
+    column_dimensions: typing.Dict[str, ColumnDimension]
