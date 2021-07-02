5e3c9a213ff7caafcffc4ffac57da82600e6e523
Vito De Tullio <vito.detullio@gmail.com>
2021-07-02 13:00:37 +0200

    clean

--- pdf2xls/writer/csvwriter.py
+++ pdf2xls/writer/csvwriter.py
@@ -13,12 +13,22 @@ from .abcwriter import ABCWriter
 MONTH: Final = 'month'
 
 
+def clean(descrizione: str) -> str:
+    for prefix in ('AD.COM.LE DA TR. NEL ',
+                   'AD.REG.LE DA TR. NEL ',
+                   'TICKET PASTO'):
+        if descrizione.startswith(prefix):
+            return f'{prefix}*'
+
+    return descrizione
+
+
 def fieldnames(infos: List[Info]) -> List[str]:
     return ([MONTH] +
             [column_header.name
              for column_header in ColumnHeader
              if column_header is not ColumnHeader.detail] +
-            list(sorted({ad.descrizione
+            list(sorted({clean(ad.descrizione)
                          for info in infos
                          for ad in info.additional_details})))
 
@@ -28,7 +38,7 @@ def rows(infos: List[Info]) -> List[Dict[str, Union[date, Decimal]]]:
              **{c.header.name: c.howmuch
                 for c in info.columns
                 if c.howmuch is not None},
-             **{ad.descrizione: ad.competenze - ad.trattenute
+             **{clean(ad.descrizione): ad.competenze - ad.trattenute
                 for ad in info.additional_details}}
             for info in infos]
 
