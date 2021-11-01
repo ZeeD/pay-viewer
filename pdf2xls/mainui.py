c7f64153cde3b3edf49db4533743807fe4e9ff04
Vito De Tullio <vito.detullio@gmail.com>
2021-11-01 18:17:38 +0100

    save

--- pdf2xls/mainui.py
+++ pdf2xls/mainui.py
@@ -1,43 +1,84 @@
-from PySide6.QtWidgets import QApplication
+from datetime import date
+from typing import List
+
+from PySide6.QtCore import QSettings
+from PySide6.QtGui import QStandardItemModel
 from PySide6.QtUiTools import QUiLoader
-from pkg_resources import resource_filename
-from PySide6.QtGui import QStandardItemModel, QStandardItem, QWindow
+from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QFileDialog
+from PySide6.QtWidgets import QWidget
 
 from .automation import try_fetch_new_data
-from datetime import date
-
-MAINUI_UI_PATH = resource_filename('pdf2xls', 'mainui.ui')
-
-
-class open_folder:
-    def __init__(self, window: QWindow) -> None:
-        self.window = window
-
-    def __call__(self) -> None:
-        pass
+from .constants import MAINUI_UI_PATH
+from .constants import SETTINGS_DATA_PATH
+from .constants import SETTINGS_PASSWORD
+from .constants import SETTINGS_USERNAME
+from .constants import SETTINGSUI_UI_PATH
+from .loader import load
+from .viewmodel import SortFilterViewModel
 
 
-class fetch_new_data:
-    def __init__(self) -> None:
-        pass
+def new_model(settings: QSettings) -> QStandardItemModel:
+    def load_current_data() -> None:
+        infos = load(settings.value(SETTINGS_DATA_PATH))
+        model.load(infos)
 
-    def __call__(self) -> None:
+    def update() -> None:
+        print('update', settings.value(SETTINGS_DATA_PATH))
         last = date(2021, 8, 1)
-        new_data = try_fetch_new_data(last)
+        new_data = try_fetch_new_data(last, settings)
         if new_data:
-            pass  # TODO reload ui? boh
+            load_current_data()
+
+    model = SortFilterViewModel(None, [])
+    load_current_data()
+
+    model.update = update
+    return model
+
+
+def new_settingsui(settings: QSettings) -> QWidget:
+    def save_settings() -> None:
+        settings.setValue(SETTINGS_USERNAME,
+                          settingsui.usernameLineEdit.text())
+        settings.setValue(SETTINGS_PASSWORD,
+                          settingsui.passwordLineEdit.text())
+        settings.setValue(SETTINGS_DATA_PATH,
+                          settingsui.dataPathLineEdit.text())
+
+    def open_folder() -> None:
+        settingsui.dataPathLineEdit.setText(QFileDialog.getExistingDirectory())
+
+    settingsui = QUiLoader().load(SETTINGSUI_UI_PATH)
+    settingsui.usernameLineEdit.setText(settings.value(SETTINGS_USERNAME))
+    settingsui.passwordLineEdit.setText(settings.value(SETTINGS_PASSWORD))
+    settingsui.dataPathLineEdit.setText(settings.value(SETTINGS_DATA_PATH))
+
+    settingsui.buttonBox.accepted.connect(save_settings)
+    settingsui.toolButton.clicked.connect(open_folder)
+
+    return settingsui
+
+
+def new_mainui(model: QStandardItemModel, settingsui: QWidget) -> QWidget:
+    mainui = QUiLoader().load(MAINUI_UI_PATH)
+    mainui.tableView.setModel(model)
+    mainui.show()
+
+    mainui.actionUpdate.triggered.connect(model.update)
+    mainui.actionSettings.triggered.connect(settingsui.show)
+    settingsui.accepted.connect(model.update)
+
+    return mainui
 
 
 def main_ui() -> int:
-    model = QStandardItemModel(14, 3)
-    for row in range(model.rowCount()):
-        for column in range(model.columnCount()):
-            model.setItem(row, column, QStandardItem(f'{row=}, {column=}'))
-
     app = QApplication([__file__])
-    window = QUiLoader().load(MAINUI_UI_PATH)
-    window.tableView.setModel(model)
-    window.actionOpen_folder.triggered.connect(open_folder(window))
-    window.actionFetch_new_data.triggered.connect(fetch_new_data())
-    window.show()
+
+    settings = QSettings('ZeeD', 'pdf2xls')
+    model = new_model(settings)
+    settingsui = new_settingsui(settings)
+    mainui = new_mainui(model, settingsui)
+    mainui = mainui
+
     return app.exec_()
