3c2ac6b49dcaa7e7d6e4952e1f9e2268dfe85bee
Vito De Tullio <vito.detullio@gmail.com>
2022-02-15 18:55:39 +0100

    x

--- demo/main.py
+++ demo/main.py
@@ -1,4 +1,5 @@
 from PySide6.QtCore import QUrl
+from PySide6.QtQuick import QQuickItem
 from PySide6.QtQuick import QQuickView
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QDialog
@@ -8,13 +9,13 @@ from PySide6.QtWidgets import QWidget
 
 
 class dump:
-    def __init__(self, view):
-        self.view = view
+    def __init__(self, qqv: QQuickView):
+        self.qqv = qqv
 
-    def __call__(self, status):
+    def __call__(self, status: QQuickView.Status) -> None:
         print(f'{status=}')
         if status is QQuickView.Error:
-            for error in self.view.errors():
+            for error in self.qqv.errors():
                 print(f'{error=}')
 
 
@@ -31,13 +32,15 @@ view = QQuickView()
 view.statusChanged.connect(dump(view))
 view.setResizeMode(QQuickView.SizeRootObjectToView)
 view.setSource(QUrl.fromLocalFile('view.qml'))
+range_slider: QQuickItem = view.rootObject()
+range_slider.first_moved.connect(lambda first_value: print(f'{first_value=}'))
+range_slider.second_moved.connect(lambda second_value: print(f'{second_value=}'))
+range_slider.setProperty('from', 0.)
+range_slider.setProperty('to', 1000.)
 container = QWidget.createWindowContainer(view)
 container.setMinimumSize(100, 10)
 layout.addWidget(container)
 
-range_slider = view.rootObject()
-range_slider.first_moved.connect(lambda first_value: print(f'{first_value=}'))
-range_slider.second_moved.connect(lambda second_value: print(f'{second_value=}'))
 
 dialog.show()
 raise SystemExit(app.exec())
--- demo/view.qml
+++ demo/view.qml
@@ -5,11 +5,6 @@ RangeSlider {
     signal first_moved(real first_value)
     signal second_moved(real second_value)
 
-    from: 1
-    to: 100
-    first.value: 25
-    second.value: 75
-
     first.onMoved: first_moved(first.value)
     second.onMoved: second_moved(second.value)
 }
