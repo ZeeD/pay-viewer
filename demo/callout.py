5d80aef7c3517bb857ff902fa9823c3bf2b2d3b8
Vito De Tullio <vito.detullio@gmail.com>
2022-10-15 11:20:05 +0200

    updated callout

--- demo/callout.py
+++ demo/callout.py
@@ -1,8 +1,14 @@
+from decimal import Decimal
+from typing import cast
+
 from PySide6.QtCharts import QChart, QLineSeries
 from PySide6.QtCharts import QChartView
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QGraphicsItem
+from PySide6.QtWidgets import QGraphicsLayoutItem
+from PySide6.QtWidgets import QGraphicsLinearLayout
 from PySide6.QtWidgets import QGraphicsProxyWidget
 from PySide6.QtWidgets import QGraphicsWidget
 from PySide6.QtWidgets import QLabel
@@ -12,10 +18,19 @@ class ChartHover(QGraphicsWidget):
 
     def __init__(self, parent: QGraphicsItem | None=None) -> None:
         super().__init__(parent)
-        self.proxy = QGraphicsProxyWidget(self)
+        self.items: list[QGraphicsLayoutItem] = []
+        self.setLayout(QGraphicsLinearLayout(Qt.Vertical))
 
-    def settext(self, text: str) -> None:
-        self.proxy.setWidget(QLabel(text))
+    def set_howmuchs(self, howmuchs: dict[str, Decimal]) -> None:
+        layout = cast(QGraphicsLinearLayout, self.layout())
+        for item in self.items:
+            layout.removeItem(item)
+        self.items.clear()
+        for name, howmuch in howmuchs.items():
+            item = QGraphicsProxyWidget(self)
+            item.setWidget(QLabel(f'{name}: {howmuch}'))
+            layout.addItem(item)
+            self.items.append(item)
 
 
 class Chart(QChart):
@@ -23,6 +38,7 @@ class Chart(QChart):
     def __init__(self) -> None:
         super().__init__()
         self.createDefaultAxes()
+        self.legend().hide()
 
         series = QLineSeries(self)
         series.append(1, 3)
@@ -40,13 +56,20 @@ class ChartView(QChartView):
 
         chart = Chart()
         self.setChart(chart)
-        self.setMouseTracking(True)
-
         self.hover = ChartHover(chart)
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        chart = self.chart()
+        if not chart:
+            raise Exception('no chart!')
         pos = event.position()
-        self.hover.settext(f'X: {pos.x():.2f} \nY: {pos.y():.2f} ')
+        value = chart.mapToValue(pos)
+        self.hover.set_howmuchs({
+            'x': Decimal.from_float(pos.x()),
+            'y': Decimal.from_float(pos.y()),
+            'ex': Decimal(value.x()),
+            'ey': Decimal(value.y())
+        })
         self.hover.setPos(pos)
         super().mouseMoveEvent(event)
 
